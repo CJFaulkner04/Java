1c5ab30792e016f73b9652ef7fcf512f2e930c7f
Carlos Faulkner <drcjfaulkner@gmail.com>
2023-05-10 21:57:13 -0400

    ChatBot

---
+++ ChatBot.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args){
+        ArrayList<Integer> integers = new ArrayList<Integer>();
+        Scanner input = new Scanner(System.in);
+    }
+
+    public static String Greeting(){
+
+        return "";
+    }
+}
